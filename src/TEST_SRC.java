31747209909df5953430f74050ae59d333e9e21b
stran <stran@DESKTOP-A7K9QLI>
2018-04-11 14:02:06 +0900

    2nd commit hisashiburi no java

--- src/TEST_SRC.java
+++ src/TEST_SRC.java
@@ -4,6 +4,7 @@ public class TEST_SRC {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World!!");
+		System.out.println("久しぶりのじゃばじゃば('ω')");
 	}
 
 }
